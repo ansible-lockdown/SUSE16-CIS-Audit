72db4bea3a05d21ce5a7c4893c23ade304e9da2a
Mark Bolwell <mark.bollyuk@gmail.com>
2026-09-21 16:08:09 +0100

    Alignment and  improvements
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- run_audit.sh
+++ run_audit.sh
@@ -21,9 +21,10 @@
 # January 2025  Added Suse OS discovery
 # May 2025      Added formation typos to help and fixed some typos
 # Sept25        Added Additional max concurrent process option
+# June 2026     Changed OS discovery to use BENCHMARK_OS variable to allow for future OS discovery and audit alignment
+# July 2026     Updated goss version discovery
 # Variables in upper case tend to be able to be adjusted
 # lower case variables are discovered or built from other variables
-# June 2026     Changed OS discovery to use BENCHMARK_OS variable to allow for future OS discovery and audit alignment
 
 # Goss benchmark variables (these should not need changing unless new release)
 BENCHMARK=CIS # Benchmark Name aligns to the audit
@@ -80,7 +81,7 @@ done
 
 # check access need to run as root or privileges due to some configuration access
 if [ "$(/usr/bin/id -u)" -ne 0 ]; then
-  echo "Script need to run with root privileges"
+  echo "Script needs to run with root privileges"
   exit 1
 fi
 
@@ -151,12 +152,12 @@ echo
 export FAILURE=0
 if [ -s "${AUDIT_BIN}" ]; then
   echo "OK - Audit binary $AUDIT_BIN is available"
-  goss_installed_version="$($AUDIT_BIN -v | awk '{print $NF}' | cut -dv -f2)"
+  goss_installed_version="$($AUDIT_BIN -v | awk 'NR==1{print $NF}' | cut -dv -f2)"
   newer_version=$(echo -e "$goss_installed_version\n$AUDIT_BIN_MIN_VER" | sort -V | tail -n 1)
   if [ "$goss_installed_version" = "$newer_version" ] || [ "$goss_installed_version" = "$AUDIT_BIN_MIN_VER" ]; then
     echo "OK - Goss is installed and version is ok ($goss_installed_version >= $AUDIT_BIN_MIN_VER)"
   else
-    echo "WARNING - Goss installed = ${goss_installed_version}, does not met minimum of ${AUDIT_BIN_MIN_VER}"
+    echo "WARNING - Goss installed = ${goss_installed_version}, does not meet minimum of ${AUDIT_BIN_MIN_VER}"
     export FAILURE=2
   fi
 else
